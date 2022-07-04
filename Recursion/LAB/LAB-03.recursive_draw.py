302db9d8a9605cebbb6aea4f0030e4854aa8ec76
Azzarox <62171309+Azzarox@users.noreply.github.com>
2022-07-04 16:53:19 +0300

    updated with lecturer solution

--- Recursion/LAB/LAB-03.recursive_draw.py
+++ Recursion/LAB/LAB-03.recursive_draw.py
@@ -1,26 +1,46 @@
-number = int(input())
-
+# My way of solving the problem:
 
 # It starts with draw_figure and going downwards ( from 5 to 1 for example )
 # When hits 1 it prints the last row of figure and then calls the next function
 # Which starts from the last number (which was 1) and goes upwards toward 5
 
-def draw(initial_value_n):
-    def draw_figure(max_n):
-        if max_n == 1:
-            print("*" * max_n)
-            return draw_figure_hash(1, initial_value_n)
-        print("*" * max_n)
-        return draw_figure(max_n - 1)
+# number = int(input())
 
-    def draw_figure_hash(n, max_n):
-        if n == max_n:
-            print('#' * max_n)
-            return
-        print("#" * n)
-        return draw_figure_hash(n + 1, max_n)
+# def draw(initial_value_n):
+#     def draw_figure(max_n):
+#         if max_n == 1:
+#             print("*" * max_n)
+#             return draw_figure_hash(1, initial_value_n)
+#         print("*" * max_n)
+#         return draw_figure(max_n - 1)
+# 
+#     def draw_figure_hash(n, max_n):
+#         if n == max_n:
+#             print('#' * max_n)
+#             return
+#         print("#" * n)
+#         return draw_figure_hash(n + 1, max_n)
+# 
+#     draw_figure(initial_value_n)
+# 
+# 
+# draw(number)
 
-    draw_figure(initial_value_n)
+
+# From lecture
+
+n = int(input())
 
 
-draw(number)
+def draw_figure(n):
+    if n == 0:
+        return
+    print(n * "*")  # pre actions
+    draw_figure(n - 1)  # recursive call
+    print(n * "#")  # post actions
+
+
+# after the call is done it continues to the post actions which is the printing of hashtags
+
+
+draw_figure(n)
