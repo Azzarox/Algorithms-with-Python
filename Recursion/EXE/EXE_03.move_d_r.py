e8c77fad89d46420a7dec1bb9d846cac1bc4b981
Azzarox <62171309+Azzarox@users.noreply.github.com>
2022-07-18 10:30:49 +0300

    added lecturer solution

--- Recursion/EXE/EXE_03.move_d_r.py
+++ Recursion/EXE/EXE_03.move_d_r.py
@@ -1,43 +1,66 @@
+# rows = int(input())
+# cols = int(input())
+# #
+# matrix = [["-" for _ in range(cols)] for _ in range(rows)]
+#
+#
+# # print(matrix)
+# # def print_matrix(matrix):
+# #     for row in matrix:
+# #         print(' '.join(row))
+#
+#
+# end_row_index = rows - 1
+# end_cols_index = cols - 1
+# matrix[end_row_index][end_cols_index] = 'e'
+#
+#
+# solutions = []
+# def move(row, col, matrix):
+#     if row >= len(matrix) or col >= len(matrix[0]):
+#         return
+#
+#     if matrix[row][col] == "v":
+#         return
+#
+#     if matrix[row][col] == 'e':
+#         solutions.append(matrix)
+#
+#         # print_matrix(matrix)
+#         # print()
+#         return
+#
+#     matrix[row][col] = "v"
+#
+#     move(row, col + 1, matrix)
+#     move(row + 1, col, matrix)
+#
+#     matrix[row][col] = '-'
+#
+#     return len(solutions)
+#
+#
+# print(move(0, 0, matrix))
+
+
+# lector solution
+
 rows = int(input())
 cols = int(input())
-#
-matrix = [["-" for _ in range(cols)] for _ in range(rows)]
 
 
-# print(matrix)
-# def print_matrix(matrix):
-#     for row in matrix:
-#         print(' '.join(row))
+def count_all_paths(row, col, rows, cols):
+    if row >= rows or col >= cols:
+        return 0
+
+    if row == rows - 1 and col == cols - 1:
+        return 1
+
+    result = 0
+    result += count_all_paths(row + 1, col, rows, cols)  # because returns 1 when viable path
+    result += count_all_paths(row, col + 1, rows, cols)  # because returns 1 when viable path
+
+    return result
 
 
-end_row_index = rows - 1
-end_cols_index = cols - 1
-matrix[end_row_index][end_cols_index] = 'e'
-
-
-solutions = []
-def move(row, col, matrix):
-    if row >= len(matrix) or col >= len(matrix[0]):
-        return
-
-    if matrix[row][col] == "v":
-        return
-
-    if matrix[row][col] == 'e':
-        solutions.append(matrix)
-
-        # print_matrix(matrix)
-        # print()
-        return
-
-    matrix[row][col] = "v"
-
-    move(row, col + 1, matrix)
-    move(row + 1, col, matrix)
-
-    matrix[row][col] = '-'
-
-    return len(solutions)
-
-
-print(move(0, 0, matrix))
+print(count_all_paths(0, 0, rows, cols))
